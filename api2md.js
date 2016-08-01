967055377f6c183bfa81a7e6bc379cb1e88841be
mmillet <1007650009@qq.com>
2016-08-01 17:13:59 +0800

    init

---
+++ api2md.js
@@ -0,0 +1,12 @@
+/**
+ * Updated by zhengguo.chen on 2016/7/30.
+ */
+var path = require('path');
+var fs = require('fs');
+var mock = require("mockjs");
+
+const prefix = '';
+
+var api = require(apiPath);
+var apiPath = path.join(__dirname, './api.config.js');
+
---
+++ server.js
@@ -0,0 +1,81 @@
+/**
+ * Created by chenzhengguo on 2015/7/30.
+ * Updated by chenzhengguo on 2016/1/28.
+ * Updated by zhengguo.chen on 2016/6/30.
+ */
+var path = require('path');
+var fs = require('fs');
+var mock = require("mockjs");
+var app = require('express')();
+var proxy = require('express-http-proxy');
+
+var port = process.argv.slice(2)[0] || 8080;
+
+const API_FILE = path.join(__dirname, './api.config.js');
+
+var api = require(API_FILE);
+
+//查看API管理
+//静态目录
+var express = require('express');
+app.use('', express.static(path.join(__dirname, './asset')));
+app.use('/~m/cp', (req, res) => {
+  // 创建读取流
+  var readable = fs.createReadStream(path.join(__dirname, './api.config.js'));
+  // 创建写入流
+  var writable = fs.createWriteStream(path.join(__dirname, './asset/api.config.js'));
+  // 通过管道来传输流
+  readable.pipe(writable);
+  res.send('OK');
+});
+app.use('/~m/all', (req, res) => {
+  res.contentType("application/json; charset=UTF-8");
+  fs.stat(API_FILE, (err, state) => {
+    res.json(Object.assign(api, {lastModify: state.mtime.getTime()}));
+  });
+});
+app.use('/~m/download', (req, res) => {
+  res.contentType('application/javascript; charset=UTF-8');
+  res.sendFile(path.join(__dirname, './api.config.js'));
+});
+
+// 支持jsonp
+app.set('jsonp callback name', 'callback');
+
+// 注册API
+api.groups.forEach(group => {
+  group.data.forEach(reqData => {
+    if(reqData.deprecated) return;
+    reqData.forward ?
+      app.use(api.prefix + reqData.url,
+        proxy('localhost:' + port, {
+          forwardPath: () => {
+            console.log(`Forward => ${api.prefix + reqData.forward}`);
+            return api.prefix + reqData.forward;
+          }
+        })
+      )
+      :
+      app.use(api.prefix + reqData.url, (req, res) => {
+        console.log(`[${req.ip}] Request: ${decodeURIComponent(req.originalUrl)}`);
+        var apiRes = mock.mock(reqData.res);
+        res.set('Access-Control-Allow-Origin', '*');
+        res.set('Access-Control-Allow-Credentials', true);
+        res.set('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+        res.set('Access-Control-Allow-Headers', 'x-requested-with, content-type, accept, origin, authorization, x-csrftoken, user-agent, accept-encoding');
+        if(req.method !== 'OPTIONS' && (reqData.status && reqData.status !== 200)) {
+          res.sendStatus(reqData.status);
+        }
+        setTimeout(() => res.jsonp(apiRes), reqData.delay || 0);
+      });
+  })
+});
+
+// 404
+app.use((req, res) => {
+  res.sendStatus(404);
+});
+
+var server = app.listen(port, () => {
+  console.info('Mock server is listening at ' + port);
+});
